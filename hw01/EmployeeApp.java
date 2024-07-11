28c067a8ea4185bdf268ca6a0940a8034759f90d
Jumulleong <wnanffjd@gmail.com>
2024-07-12 02:11:52 +0900

    selfstudy

--- hw01/EmployeeApp.java
+++ hw01/EmployeeApp.java
@@ -11,15 +11,15 @@ public class EmployeeApp {
 
 
 
-
-
     public static void main(String[] args) {
 
-        Employee[] employee = new lab04.Employee[MAXNUM];
-        int option = 0; //
-        Scanner scanner = new Scanner(System.in);
+        Employee[] employee = new Employee[MAXNUM]; // 객체 생성
 
-        //배열 원소마다 객체 생성
+        int option = 0; // 메뉴 선택값 정의
+
+        Scanner scanner = new Scanner(System.in); // 스캐너 객체 생성
+
+        // for 사용하여 배열 원소마다 객체 생성
         for(int i = 0; i< MAXNUM; i++) {
             employee[i] = new Employee();
             // Employee가 클래스로서 매개변수를 가지니까()가 있는거,
@@ -27,10 +27,26 @@ public class EmployeeApp {
             // employee는 이미 위에서 타입을 명시했기 떄문에 이번엔 타입 명시 안해도 되는것
         }
 
-        staffs[0].setName("John",	"Smith");
+        // 배열 원소마다 값 넣기
+
+        // Staffs #1
+        staffs[0].setName("John","Smith");
         staffs[0].setAge(25);
         staffs[0].setPosition("Newcomer");
         staffs[0].setSalary(300);
+
+        // Staffs #2
+        staffs[0].setName("John","Smith");
+        staffs[0].setAge(25);
+        staffs[0].setPosition("Newcomer");
+        staffs[0].setSalary(300);
+
+        // Staffs #3
+        staffs[0].setName("John","Smith");
+        staffs[0].setAge(25);
+        staffs[0].setPosition("Newcomer");
+        staffs[0].setSalary(300);
+
         engineers[0].setName("Bob",	"Lewis");
         engineers[0].setAge(28);
         engineers[0].setPosition("Junior	Engineer");
--- selfstudy/note.java
+++ selfstudy/note.java
@@ -3,5 +3,13 @@ package selfstudy;
 public class note {
     public static void main(String[] args) {
         System.out.println("개발자가 되기 위한 필수 개발 언어 Java");
+
+        int x = 0b1010; // x는 8+2+1 = 11
+        boolean result = (x == 20);
+        boolean result = (x != 20);
+        boolean result = (x>20);
+
+
+        System.out.println(x);
     }
 }
